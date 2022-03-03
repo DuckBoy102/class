146e9424f152b94eff9611d340381a6544ae1192
DuckBoy102 <avieln7@gmail.com>
2022-03-03 16:14:09 +0200

    hey

--- ConsoleApp1/Ball.cs
+++ ConsoleApp1/Ball.cs
@@ -12,36 +12,43 @@ namespace ConsoleApp1
         string color;
         string type;
         string fabric;
-        public int get_size()
-        {
-            return this.size;
-        }
-        public void set_size(int size)
+        //public int get_size()
+        //{
+            //return this.size;
+        //}
+        //public void set_size(int size)
+        //{
+            //this.size = size;
+        //}
+        //public string get_color()
+        //{
+            //return this.color;
+        //}
+        //public void set_color(string color)
+        //{
+            //this.color = color;
+        //}
+        //public string get_type()
+        //{
+            //return this.type;
+        //}
+        //public void set_type(string type)
+        //{
+            //this.type = type;
+        //}
+        //public string get_fabric()
+        //{
+            //return this.fabric;
+        //}
+        //public void set_fabric(string fabric)
+        //{
+            //this.fabric = fabric;
+        //}
+        public Ball (int size, string color, string type, string fabric)
         {
             this.size = size;
-        }
-        public string get_color()
-        {
-            return this.color;
-        }
-        public void set_color(string color)
-        {
             this.color = color;
-        }
-        public string get_type()
-        {
-            return this.type;
-        }
-        public void set_type(string type)
-        {
             this.type = type;
-        }
-        public string get_fabric()
-        {
-            return this.fabric;
-        }
-        public void set_fabric(string fabric)
-        {
             this.fabric = fabric;
         }
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,16 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Ball my_new_ball = new Ball();
+            //Ball my_new_ball = new Ball();
+            //Console.WriteLine("Enter your ball detail");
+            //Console.WriteLine("size?");
+            //my_new_ball.size = int.Parse(Console.ReadLine());
+            //Console.WriteLine("type?");
+            //my_new_ball.type = Console.ReadLine();
+            //Console.WriteLine("color?");
+            //my_new_ball.color = Console.ReadLine();
+            //Console.WriteLine("fabric?");
+            //my_new_ball.fabric = Console.ReadLine();
             Console.WriteLine("Enter your ball detail");
             Console.WriteLine("size?");
-            my_new_ball.set_size (int.Parse(Console.ReadLine()));
+            int size = int.Parse(Console.ReadLine());
             Console.WriteLine("color?");
-            my_new_ball.set_color(Console.ReadLine());
+            string color = Console.ReadLine();
             Console.WriteLine("type?");
-            my_new_ball.set_type(Console.ReadLine());
+            string type = Console.ReadLine();
             Console.WriteLine("fabric?");
-            my_new_ball.set_fabric(Console.ReadLine());
+            string fabric = Console.ReadLine();
+            Ball my_new_ball = new Ball(size, color, type, fabric);
         }
     }
 }
